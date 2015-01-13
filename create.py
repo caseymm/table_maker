bd655be03ffb357efa32c83a66a55701e75a7a99
caseymm <caseymm@gmail.com>
2015-01-12 23:06:43 -0500

    added basic clause for date and number

--- create.py
+++ create.py
@@ -11,7 +11,7 @@ def slugify(keyword):
 print"""
                                                                   *
                                                                  /|
-_____ _   ___ _    ___  __      _____ ____  _   ___ ___         / |
+_____ _   ___  _    ___  __      _____ ____  _   ___ ___        / |
 |_   _/_\ | _ ) |  | __| \ \    / /_ _|_  / /_\ | _ \   \      /  |
   | |/ _ \| _ \ |__| _|   \ \/\/ / | | / / / _ \|   / |) |   _/___|_
   |_/_/ \_\___/____|___|   \_/\_/ |___/___/_/ \_\_|_\___/  ( ______ )
@@ -42,23 +42,20 @@ excel_file = os.path.abspath(excel_file_path.strip(' '))
 
 book = open_workbook(excel_file)
 sheet = book.sheet_by_index(0)
-# print sheet.name
-# print sheet.nrows
-# print sheet.ncols
 
-# for col_index in range(sheet.ncols):
-#     print col_index
 print "Here is a example item from your spreadsheet."
 print
 
+check_float = []
 for row_index in range(sheet.nrows)[starting_row:starting_row+1]:
-    # print row_index
     for col_index in range(sheet.ncols):
-        # print cellname(row_index,col_index),'-',
         print col_index, sheet.cell(row_index,col_index).value
-        if isinstance(sheet.cell(row_index,col_index).value, float):
-            print '    this is a floated number'
 
+        # Notes that the value is not a string and appends it to a list to be referenced later
+        if isinstance(sheet.cell(row_index,col_index).value, float):
+            check_float.append(col_index)
+
+# Allows the user to decide which columns from the spreadsheet to keep
 print
 print "Please enter the index number of the columns you would like to keep. You may also type 'all' if you would like to keep all columns."
 print "Format your response like this: 2,4,5,8"
@@ -70,38 +67,44 @@ if keep_cols == 'all':
 else:
     keep_cols_list = [int(i) for i in keep_cols.split(',')]
 
+# Section where the user names the columns that he or she has kept and is prompted to decide if they contain a number or date
 col_name_dict = {}
 for i in keep_cols_list:
     ival = raw_input ("Column "+str(i)+" name: ")
-    col_name_dict.setdefault(i, ival)
-
-    # Should really check for the different types & allow user to set formatting here
+    date_or_num = "string"
+    if i in check_float:
+        print "It looks like the "+ ival +" column contains dates or numbers. If you would like to render this data as a date, type: 'date', for a number, type 'number.' Hit enter if neither."
+        date_or_num = raw_input(">>> ")
+    col_name_dict.setdefault(i, [ival, date_or_num])
 
 json_list = []
 
 if ending_row == 'end':
     ending_row = sheet.nrows-1
 
+# Generates json with correctly formatted values (string, date, number, etc.) based on the info entered above
 for row_index in range(sheet.nrows)[starting_row:ending_row]:
-# for row_index in range(sheet.nrows):
     tmp = {}
     for col_index in col_name_dict:
-        cell_val = sheet.cell(row_index,col_index).value
+        if col_name_dict[col_index][1] == 'date':
+            try:
+                formatted_dt = datetime(*xlrd.xldate_as_tuple(cell_val, book.datemode))
+                # put in an if statement here that will allow the user to decide how to format the date
+                cell_val = datetime.strftime(formatted_dt, '%x')
+            except:
+                pass
+        # elif col_name_dict[col_index][1] == 'number':
+        # add more formatting options here for decimal places
+        # also for $ and % - check *100 for percentages
+        else:
+            cell_val = sheet.cell(row_index,col_index).value
 
-        # This needs to be moved and separated from other floated numbers otherwise all will become dates
-        try:
-            formatted_dt = datetime(*xlrd.xldate_as_tuple(cell_val, book.datemode))
-            # put in an if statement here that will allow the user to decide how to format the date
-            cell_val = datetime.strftime(formatted_dt, '%x')
-        except:
-            pass
-
-        tmp.setdefault(col_name_dict[col_index], cell_val)
+        tmp.setdefault(col_name_dict[col_index][0], cell_val)
     json_list.append(tmp)
 
 json_name = raw_input ('Enter a name for your json:')
 print
-# print json.dumps(json_list)
+
 to_json=open(json_name+'.json','w')
 jsonified = json.dumps(json_list)
 print>>to_json, jsonified
@@ -112,7 +115,7 @@ open_js = open('js/script_new.js','w')
 for ln in open_template:
     match = re.search(r"TABLE_HEADER", ln)
     if match:
-        ln = ln.replace(match.group(0), "var col_list = "+str(list(col_name_dict[i] for i in col_name_dict))+";")
+        ln = ln.replace(match.group(0), "var col_list = "+str(list(col_name_dict[i][0] for i in col_name_dict))+";")
 
     match = re.search(r"json_url_here", ln)
     if match:
@@ -123,8 +126,8 @@ for ln in open_template:
         ln = ln.replace(match.group(0), "")
         write_it = ''
         for t in col_name_dict:
-            open_js.write("var "+slugify(col_name_dict[t])+" = '<td>'+element['"+col_name_dict[t]+"']+'</td>';\n")
-            write_it += slugify(col_name_dict[t])+'+'
+            open_js.write("var "+slugify(col_name_dict[t][0])+" = '<td>'+element['"+col_name_dict[t][0]+"']+'</td>';\n")
+            write_it += slugify(col_name_dict[t][0])+'+'
         open_js.write("$('tbody').append('<tr>'+"+write_it+"'</tr>');")
 
     open_js.write(ln)
--- js/script_new.js
+++ js/script_new.js
@@ -1,6 +1,6 @@
 $(document).ready( function () {
 
-    var col_list = ['name', 'team', 'number'];
+    var col_list = ['name', 'team', 'number', 'date'];
     for (i in col_list){
         $('.table-header').append('<th title="Field #'+i.split(' ').join('')+'">'+col_list[i]+'</th>');
     }
@@ -18,7 +18,8 @@ function get_data(){
 var name = '<td>'+element['name']+'</td>';
 var team = '<td>'+element['team']+'</td>';
 var number = '<td>'+element['number']+'</td>';
-$('tbody').append('<tr>'+name+team+number+'</tr>');                      
+var date = '<td>'+element['date']+'</td>';
+$('tbody').append('<tr>'+name+team+number+date+'</tr>');                      
 
                   })
                   $('#table-1').dataTable({
